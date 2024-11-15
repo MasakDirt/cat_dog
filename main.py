f34237bdec05109fd1a93636416bd81a13f5aefb
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-16 00:35:33 +0200

    refactor main file with templates

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.templating import Jinja2Templates
+
+from cat_dog import routers as cat_dog_routers
+
 
 app = FastAPI()
 
+app.include_router(cat_dog_routers.router)
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html")
